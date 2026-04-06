ca9a09c043e0e6220da48312f1861a985682a163
nastonish <161249729+nastonish@users.noreply.github.com>
2026-04-06 11:44:43 +0300

    Implement analytic geometry DLL API and demo integration

--- GeometryDLL/GeometryDLL.cpp
+++ GeometryDLL/GeometryDLL.cpp
@@ -1,100 +1,98 @@
 #include "GeometryDLL.h"
 
+#include <cmath>
+
 double __stdcall distance2d(double x1, double y1, double x2, double y2)
 {
-    (void)x1;
-    (void)y1;
-    (void)x2;
-    (void)y2;
-    return 0.0;
+    double dx = x2 - x1;
+    double dy = y2 - y1;
+    return std::sqrt(dx * dx + dy * dy);
 }
 
 int __stdcall midpoint2d(double x1, double y1, double x2, double y2, double* outX, double* outY)
 {
-    (void)x1;
-    (void)y1;
-    (void)x2;
-    (void)y2;
-
     if (outX == 0 || outY == 0)
     {
         return 0;
     }
 
-    *outX = 0.0;
-    *outY = 0.0;
+    *outX = (x1 + x2) / 2.0;
+    *outY = (y1 + y2) / 2.0;
     return 1;
 }
 
 double __stdcall vectorLength2d(double x, double y)
 {
-    (void)x;
-    (void)y;
-    return 0.0;
+    return std::sqrt(x * x + y * y);
 }
 
 double __stdcall dotProduct2d(double ax, double ay, double bx, double by)
 {
-    (void)ax;
-    (void)ay;
-    (void)bx;
-    (void)by;
-    return 0.0;
+    return ax * bx + ay * by;
 }
 
 double __stdcall crossProduct2d(double ax, double ay, double bx, double by)
 {
-    (void)ax;
-    (void)ay;
-    (void)bx;
-    (void)by;
-    return 0.0;
+    return ax * by - ay * bx;
 }
 
 double __stdcall triangleArea2d(double x1, double y1, double x2, double y2, double x3, double y3)
 {
-    (void)x1;
-    (void)y1;
-    (void)x2;
-    (void)y2;
-    (void)x3;
-    (void)y3;
-    return 0.0;
+    double area2 = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+    return std::fabs(area2) / 2.0;
 }
 
 int __stdcall lineFromPoints2d(double x1, double y1, double x2, double y2, double* outA, double* outB, double* outC)
 {
-    (void)x1;
-    (void)y1;
-    (void)x2;
-    (void)y2;
-
     if (outA == 0 || outB == 0 || outC == 0)
     {
         return 0;
     }
 
-    *outA = 0.0;
-    *outB = 0.0;
-    *outC = 0.0;
+    if (x1 == x2 && y1 == y2)
+    {
+        return 0;
+    }
+
+    *outA = y1 - y2;
+    *outB = x2 - x1;
+    *outC = x1 * y2 - x2 * y1;
     return 1;
 }
 
 double __stdcall pointLineDistance2d(double px, double py, double a, double b, double c)
 {
-    (void)px;
-    (void)py;
-    (void)a;
-    (void)b;
-    (void)c;
-    return 0.0;
+    double denominator = std::sqrt(a * a + b * b);
+    if (denominator == 0.0)
+    {
+        return -1.0;
+    }
+
+    double numerator = std::fabs(a * px + b * py + c);
+    return numerator / denominator;
 }
 
 double __stdcall angleBetweenVectorsDeg2d(double ax, double ay, double bx, double by)
 {
-    (void)ax;
-    (void)ay;
-    (void)bx;
-    (void)by;
-    return 0.0;
+    const double pi = 3.14159265358979323846;
+    double lenA = vectorLength2d(ax, ay);
+    double lenB = vectorLength2d(bx, by);
+
+    if (lenA == 0.0 || lenB == 0.0)
+    {
+        return -1.0;
+    }
+
+    double cosineValue = dotProduct2d(ax, ay, bx, by) / (lenA * lenB);
+
+    if (cosineValue > 1.0)
+    {
+        cosineValue = 1.0;
+    }
+    else if (cosineValue < -1.0)
+    {
+        cosineValue = -1.0;
+    }
+
+    return std::acos(cosineValue) * 180.0 / pi;
 }
--- GeometryDLL/GeometryDLL.h
+++ GeometryDLL/GeometryDLL.h
@@ -1,14 +1,20 @@
 #ifndef GEOMETRYDLL_H
 #define GEOMETRYDLL_H
 
-extern "C" __declspec(dllexport) double __stdcall distance2d(double x1, double y1, double x2, double y2);
-extern "C" __declspec(dllexport) int __stdcall midpoint2d(double x1, double y1, double x2, double y2, double* outX, double* outY);
-extern "C" __declspec(dllexport) double __stdcall vectorLength2d(double x, double y);
-extern "C" __declspec(dllexport) double __stdcall dotProduct2d(double ax, double ay, double bx, double by);
-extern "C" __declspec(dllexport) double __stdcall crossProduct2d(double ax, double ay, double bx, double by);
-extern "C" __declspec(dllexport) double __stdcall triangleArea2d(double x1, double y1, double x2, double y2, double x3, double y3);
-extern "C" __declspec(dllexport) int __stdcall lineFromPoints2d(double x1, double y1, double x2, double y2, double* outA, double* outB, double* outC);
-extern "C" __declspec(dllexport) double __stdcall pointLineDistance2d(double px, double py, double a, double b, double c);
-extern "C" __declspec(dllexport) double __stdcall angleBetweenVectorsDeg2d(double ax, double ay, double bx, double by);
+#ifdef GEOMETRYDLL_EXPORTS
+#define GEOMETRY_API extern "C" __declspec(dllexport)
+#else
+#define GEOMETRY_API extern "C" __declspec(dllimport)
+#endif
+
+GEOMETRY_API double __stdcall distance2d(double x1, double y1, double x2, double y2);
+GEOMETRY_API int __stdcall midpoint2d(double x1, double y1, double x2, double y2, double* outX, double* outY);
+GEOMETRY_API double __stdcall vectorLength2d(double x, double y);
+GEOMETRY_API double __stdcall dotProduct2d(double ax, double ay, double bx, double by);
+GEOMETRY_API double __stdcall crossProduct2d(double ax, double ay, double bx, double by);
+GEOMETRY_API double __stdcall triangleArea2d(double x1, double y1, double x2, double y2, double x3, double y3);
+GEOMETRY_API int __stdcall lineFromPoints2d(double x1, double y1, double x2, double y2, double* outA, double* outB, double* outC);
+GEOMETRY_API double __stdcall pointLineDistance2d(double px, double py, double a, double b, double c);
+GEOMETRY_API double __stdcall angleBetweenVectorsDeg2d(double ax, double ay, double bx, double by);
 
 #endif
